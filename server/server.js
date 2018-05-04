917d709a2f8a8b00c6b5be5da56297c8009ef2ff
0Calories <cppanand@gmail.com>
2018-05-03 23:05:41 -0400

    Added GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -94,20 +95,25 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => res.status(400).send());
 });
 
-app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-});
-
 // POST Request to create new User
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
-    
+
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
         res.header('x-auth', token).send(user);
     }).catch((e) => res.status(400).send(e));
+});
+
+// GET Request for receiving a specific User
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
